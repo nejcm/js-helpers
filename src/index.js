6c80c94c62d868dacd1acb637ca52ee7156df9ff
Nejc <nmursi2@gmail.com>
2020-03-22 20:55:39 +0100

    feat: added style helpers

--- src/index.js
+++ src/index.js
@@ -8,5 +8,6 @@ export { parse2Integer } from './lib/number';
 export { filterEmpty, flatten, get, getMultiple, toJson } from './lib/object';
 export { camelCase2Word, ellipsis, randomPassword, replace } from './lib/string';
 export { deserialize, pushHistoryState, replaceHistoryState, serialize } from './lib/url';
+export { measure2Css } from './lib/style';
 export { Enum };
 
