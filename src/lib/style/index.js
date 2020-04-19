9d9bc1e3310288ceac6c9d5f9ba75b6cba909116
Nejc <nmursi2@gmail.com>
2020-04-19 22:10:08 +0200

    fix: fixed travis build issue

--- src/lib/style/index.js
+++ src/lib/style/index.js
@@ -1,8 +1,13 @@
 /**
  *  Convert measure value to valid css
  * @param {String|Number} value Measure value
+ * @param {String} unit Measure unit (px|pt|em|rem|vh|vw ...) https://developer.mozilla.org/en-US/docs/Learn/CSS/Building_blocks/Values_and_units
  * @returns {String} Css value
  */
-export const measure2Css = (value) => {
-  return value ? (typeof value === 'string' ? value : `${value}px`) : undefined;
+export const measure2Css = (value, unit = 'px') => {
+  return value
+    ? typeof value === 'string'
+      ? value
+      : `${value}${unit}`
+    : undefined;
 };
